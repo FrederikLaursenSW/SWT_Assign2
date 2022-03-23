81757b462da08f0e1fd5166325ff365d86b24654
FrederikLaursenSW <60841888+FrederikLaursenSW@users.noreply.github.com>
2022-03-23 19:37:27 +0100

    remove include

--- LadeskabTest/ChargeControlUnitTest.cs
+++ LadeskabTest/ChargeControlUnitTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClassLibrary.Fakes;
 using ClassLibrary.Interfaces;
 using ClassLibrary.Models;
 using NSubstitute;
